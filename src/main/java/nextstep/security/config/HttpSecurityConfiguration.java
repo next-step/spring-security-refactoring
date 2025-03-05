a75d174f91e598fe658037f468f9f09eb803102d
qwe5507 <apvmffkdls@gmail.com>
2025-03-05 15:34:21 +0900

    feat: HttpSecurityConfiguration bean 등록

---
+++ src/main/java/nextstep/security/config/HttpSecurityConfiguration.java
@@ -0,0 +1,19 @@
+package nextstep.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration(proxyBeanMethods = false)
+public class HttpSecurityConfiguration {
+
+    private static final String BEAN_NAME_PREFIX = "nextstep.security.config.HttpSecurityConfiguration.";
+
+    private static final String HTTP_SECURITY_BEAN_NAME = BEAN_NAME_PREFIX + "httpSecurity";
+
+    @Bean(HTTP_SECURITY_BEAN_NAME)
+    @Scope("prototype")
+    HttpSecurity httpSecurity() throws Exception {
+        return new HttpSecurity();
+    }
+}
---
+++ src/main/java/nextstep/security/config/annotation/EnableWebSecurity.java
@@ -0,0 +1,25 @@
+package nextstep.security.config.annotation;
+
+import nextstep.security.config.HttpSecurityConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Documented
+@Import({HttpSecurityConfiguration.class })
+public @interface EnableWebSecurity {
+
+    /**
+     * Controls debugging support for Spring Security. Default is false.
+     * @return if true, enables debug support with Spring Security
+     */
+    boolean debug() default false;
+
+}
+
